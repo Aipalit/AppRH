07e8970875a002207468d83a89cf4ebcc34ab5e0
Aipalit <lucas.gomes.souza1997@gmail.com>
2025-04-06 09:22:14 -0300

    feat: Conexão com banco de dados

---
+++ src/main/java/com/AppRH/AppRH/DataConfiguration.java
@@ -0,0 +1,49 @@
+package com.AppRH.AppRH;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.GetMapping;
+
+@Configuration
+public class DataConfiguration {
+
+    @Bean // exportação de classes
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/Apprh?userTimezone=true&serverTimeZone=UTC");
+        dataSource.setUsername("root");
+        dataSource.setPassword("084860#Lucas");
+
+        return dataSource;
+
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.MYSQL);
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(true);
+        adapter.setDatabasePlatform("org.hibernate.dialect.MariaDBDialect");
+        adapter.setPrepareConnection(true);
+        return adapter;
+    }
+
+    // @Controller
+    // public class HomeController {
+    // @GetMapping("/")
+    // public String home(){
+    // return "index";
+    // }
+
+    // }
+
+}
